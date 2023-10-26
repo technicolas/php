d76a5535022719bff82c583a26b468cd901c327d
Nicolas <technicolas@gmail.com>
2023-10-27 00:27:53 +0200

    Add files via upload

--- joke.php
+++ joke.php
@@ -6,27 +6,35 @@
 	</head>
 
 	<body>
-		<a href="<?php $_SERVER['PHP_SELF']; ?>">Actualiser la page</a>
-		<?php
+		<div class="header">
+  			<h1><a href="<?php $_SERVER['PHP_SELF']; ?>">Actualiser la page</a></h1>
+		</div>
+		
+		<div class="menu">
+			<h1><a>A écrire...</a></h1>
+		</div>
 
-			// Décryptage des données provenant du fichier JSON et mise en forme pour leurs lectures dans la suite du tableau:
-			// ---------------------------------------------------------------------------------------------------------------
-			$json = file_get_contents("https://v2.jokeapi.dev/joke/Any?lang=fr&blacklistFlags=nsfw,religious,political,sexist,explicit");
-			$parsed_json = json_decode($json);
-			
-			$idBlague = $parsed_json->{"id"};
-			$question = $parsed_json->{"setup"};
-			$reponse = $parsed_json->{"delivery"};
-			
-			// Affichage de la première ligne du tableau:
-			// ------------------------------------------
-			// echo "<p class=\"date\">Date & heure: ${date_jour}";
-			echo "<p>";
-			echo "<p class=\"blague\"> $idBlague";
-      		echo "<p class=\"question\"> $question";
-      		echo "<p class=\"reponse\"> $reponse";
-		?>
+		<div class="main">
+			<?php
+
+				// Décryptage des données provenant du fichier JSON
+				$json = file_get_contents("https://v2.jokeapi.dev/joke/Any?lang=fr&blacklistFlags=nsfw,religious,political,sexist,explicit");
+				$parsed_json = json_decode($json);
+				
+				$idBlague = $parsed_json->{"id"};
+				$question = $parsed_json->{"setup"};
+				$reponse = $parsed_json->{"delivery"};
+				
+				// Affichages
+				echo "Nous sommes le " . date("d.m.Y") . " et je vous propose cette petite blague:" . "<br>";
+				echo "<p>";
+				echo "<p class=\"blague\"> $idBlague";
+	      		echo "<p class=\"question\"> $question";
+	      		echo "<p class=\"reponse\"> $reponse";
+			?>
+		</div>
 	</body>
+	
 	<footer>&copy Nicolas - <?php echo date("Y"); ?> </footer>
 </html>
 
